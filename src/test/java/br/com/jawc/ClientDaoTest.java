7d2357697cf143d0a150fa59745dee2558d903b8
jawc-05 <joaoalfredowc@gmail.com>
2026-03-30 15:04:36 +0200

    feat: adding the base DaoTest, remembering that im doing unit tests, not integration

--- src/test/java/br/com/jawc/ClientDaoTest.java
+++ src/test/java/br/com/jawc/ClientDaoTest.java
@@ -3,5 +3,38 @@
  */
 package br.com.jawc;
 
+import br.com.jawc.dao.ClientDao;
+import br.com.jawc.dao.ClientDaoMock;
+import br.com.jawc.dao.IClientDao;
+import br.com.jawc.domain.Client;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ClientDaoTest {
+
+    private IClientDao clientDao;
+    private Client client;
+
+    public ClientDaoTest() {
+        clientDao = new ClientDaoMock();
+    }
+
+    @Before
+    public void init(){
+        client = new Client();
+        client.setCpf("1234567891011");
+        client.setName("João");
+        client.setCity("Lublin");
+        client.setEnd("Ikar Dorm");
+        client.setState("Lubelskie");
+        client.setTel(48123213L);
+        client.setNum(123);
+
+        clientDao.save(client);
+    }
+
+    @Test
+    public void searchClient(){
+
+    }
 }
