1983b2ff8570323895a8845cd330e3195a167660
jawc-05 <joaoalfredowc@gmail.com>
2026-03-31 18:07:59 +0200

    feat: adding Singleton

--- src/main/java/br/com/jawc/dao/generic/GenericDao.java
+++ src/main/java/br/com/jawc/dao/generic/GenericDao.java
@@ -12,28 +12,94 @@ import java.util.List;
 
 public class GenericDao <T extends Persistence, E extends Serializable> implements IGenericDao<T, E> {
 
+    private SingletonMap singletonMap;
+
+    public abstract Class<T> getTipoClasse();
+
+    public abstract void atualiarDados(T entity, T entityCadastrado);
+
+    public GenericDAO() {
+        this.singletonMap = SingletonMap.getInstance();
+    }
+
+    public E getChave(T entity) throws TipoChaveNaoEncontradaException {
+        Field[] fields = entity.getClass().getDeclaredFields();
+        E returnValue = null;
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(TipoChave.class)) {
+                TipoChave tipoChave = field.getAnnotation(TipoChave.class);
+                String nomeMetodo = tipoChave.value();
+                try {
+                    Method method = entity.getClass().getMethod(nomeMetodo);
+                    returnValue = (E) method.invoke(entity);
+                    return returnValue;
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    //Criar exception de negócio TipoChaveNaoEncontradaException
+                    e.printStackTrace();
+                    throw new TipoChaveNaoEncontradaException("Chave principal do objeto " + entity.getClass() + " não encontrada", e);
+                }
+            }
+        }
+        if (returnValue == null) {
+            String msg = "Chave principal do objeto " + entity.getClass() + " não encontrada";
+            System.out.println("**** ERRO ****" + msg);
+            throw new TipoChaveNaoEncontradaException(msg);
+        }
+        return null;
+    }
+
     @Override
     public Boolean cadastrar(T entity) throws TipoChaveNaoEncontradaException {
-        return null;
+        //Map<Long, T> mapaInterno = this.map.get(getTipoClasse());
+        Map<E, T> mapaInterno = getMapa();
+        E chave = getChave(entity);
+        if (mapaInterno.containsKey(chave)) {
+            return false;
+        }
+
+        mapaInterno.put(chave, entity);
+        return true;
+    }
+
+    private Map<E, T> getMapa() {
+        Map<E, T> mapaInterno = (Map<E, T>) this.singletonMap.getMap().get(getTipoClasse());
+        if (mapaInterno == null) {
+            mapaInterno = new HashMap<>();
+            this.singletonMap.getMap().put(getTipoClasse(), mapaInterno);
+        }
+        return mapaInterno;
     }
 
     @Override
     public void excluir(E valor) {
-
+        //Map<Long, T> mapaInterno = this.map.get(getTipoClasse());
+        Map<E, T> mapaInterno = getMapa();
+        T objetoCadastrado = mapaInterno.get(valor);
+        if (objetoCadastrado != null) {
+            mapaInterno.remove(valor, objetoCadastrado);
+        }
     }
 
     @Override
     public void alterar(T entity) throws TipoChaveNaoEncontradaException {
-
+        Map<E, T> mapaInterno = getMapa();
+        E chave = getChave(entity);
+        T objetoCadastrado = mapaInterno.get(chave);
+        if (objetoCadastrado != null) {
+            atualiarDados(entity, objetoCadastrado);
+        }
     }
 
     @Override
     public T consultar(E valor) {
-        return null;
+        //Map<Long, T> mapaInterno = this.map.get(getTipoClasse());
+        Map<E, T> mapaInterno = getMapa();
+        return mapaInterno.get(valor);
     }
 
     @Override
     public Collection<T> buscarTodos() {
-        return List.of();
+        Map<E, T> mapaInterno = getMapa();
+        return mapaInterno.values();
     }
 }
