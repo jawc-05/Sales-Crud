76e61f89d29a7502218d375c56a88398ca605b65
jawc-05 <joaoalfredowc@gmail.com>
2026-03-31 18:49:02 +0200

    feat: adding ISellsDao

--- src/main/java/br/com/jawc/dao/ProductDao.java
+++ src/main/java/br/com/jawc/dao/ProductDao.java
@@ -1,9 +1,9 @@
 /**
  * @author jawc
  */
-package br.com.jawc.dao.generic;
+package br.com.jawc.dao;
 
-import br.com.jawc.dao.IProductDao;
+import br.com.jawc.dao.generic.GenericDao;
 import br.com.jawc.domain.Product;
 
 public class ProductDao extends GenericDao<Product, String> implements IProductDao {
