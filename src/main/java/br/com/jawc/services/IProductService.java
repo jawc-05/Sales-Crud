bb3eca9794ddf5dcf659ded15b1eb97e32bde954
jawc-05 <joaoalfredowc@gmail.com>
2026-03-31 19:08:41 +0200

    feat: adding IProducService

---
+++ src/main/java/br/com/jawc/services/IProductService.java
@@ -0,0 +1,10 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.services;
+
+import br.com.jawc.domain.Product;
+import br.com.jawc.services.generic.IGenericService;
+
+public interface IProductService extends IGenericService<Product, String> {
+}
