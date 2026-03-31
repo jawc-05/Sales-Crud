e94ebf2b95e88b3d0534de5e957f5e0c8b2e673a
jawc-05 <joaoalfredowc@gmail.com>
2026-03-31 18:06:10 +0200

    feat: adding genericDAO

--- src/main/java/br/com/jawc/dao/generic/GenericDao.java
+++ src/main/java/br/com/jawc/dao/generic/GenericDao.java
@@ -3,6 +3,37 @@
  */
 package br.com.jawc.dao.generic;
 
-public class GenericDao {
+import br.com.jawc.dao.Persistence;
+import br.com.jawc.exceptions.TipoChaveNaoEncontradaException;
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+public class GenericDao <T extends Persistence, E extends Serializable> implements IGenericDao<T, E> {
+
+    @Override
+    public Boolean cadastrar(T entity) throws TipoChaveNaoEncontradaException {
+        return null;
+    }
+
+    @Override
+    public void excluir(E valor) {
+
+    }
+
+    @Override
+    public void alterar(T entity) throws TipoChaveNaoEncontradaException {
+
+    }
+
+    @Override
+    public T consultar(E valor) {
+        return null;
+    }
+
+    @Override
+    public Collection<T> buscarTodos() {
+        return List.of();
+    }
 }
