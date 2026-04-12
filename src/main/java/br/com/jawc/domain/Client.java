d81ca0840701df9a5f0fd40f06a997e5fb37d4ca
jawc-05 <joaoalfredowc@gmail.com>
2026-04-12 23:11:32 +0200

    feat: adding Client class

---
+++ src/main/java/br/com/jawc/domain/Client.java
@@ -0,0 +1,35 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.domain;
+
+public class Client {
+
+    private String cpf;
+    private String nome;
+    private Long id;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
