8be023d69d867c9003732dfa4b957cfe36abf500
jawc-05 <joaoalfredowc@gmail.com>
2026-04-12 23:42:42 +0200

    test: adding ClientTest

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,32 @@
+import br.com.jawc.dao.ClientDao;
+import br.com.jawc.dao.IClientDao;
+import br.com.jawc.domain.Client;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jawc
+ */
+
+public class ClientTest {
+
+    @Test
+    public void signClient(){
+        IClientDao dao = new ClientDao();
+        Client client = new Client();
+        client.setCpf("01");
+        client.setNome("João Alfredo");
+
+        Integer count = dao.cadastrar(client);
+        assertTrue(count == 1);
+
+        Client clientBD = dao.search(client.getCpf());
+        assertNotNull(clientBD);
+        assertNotNull(clientBD.getId());
+        assertEquals(client.getCpf(), clientBD.getCpf());
+        assertEquals(client.getNome(), clientBD.getNome());
+
+    }
+}
