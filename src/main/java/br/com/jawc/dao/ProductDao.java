8215e4e8dc0d64d924aafc009220bdbb13d6bb3c
jawc-05 <joaoalfredowc@gmail.com>
2026-04-14 16:28:07 +0200

    feat: adding ProductDao class

---
+++ src/main/java/br/com/jawc/dao/ProductDao.java
@@ -0,0 +1,41 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.dao;
+
+import br.com.jawc.domain.Product;
+
+import java.util.List;
+
+public class ProductDao implements IProduct{
+    @Override
+    public Integer sign(Product product) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public Product search(String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Integer delete(Product productDB) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<Product> searchAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Integer update(Product product) throws Exception {
+        return 0;
+    }
+
+    private String getSqlInsert() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO tb_product (name,description) VALUES (?,?)");
+        return sb.toString();
+    }
+}
