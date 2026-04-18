89eb6b425212488d24f07ba3e7de842b1c7a066d
jawc-05 <joaoalfredowc@gmail.com>
2026-04-18 16:00:18 +0200

    fix: fixing the vendatest

--- src/test/java/br/com/jawc/dao/VendaDAOTest.java
+++ src/test/java/br/com/jawc/dao/VendaDAOTest.java
@@ -292,6 +292,7 @@ public class VendaDAOTest {
 		produto.setDescricao("Produto 1");
 		produto.setNome("Produto 1");
 		produto.setValor(valor);
+        produto.setPeso(BigDecimal.valueOf(10));
 		produtoDao.cadastrar(produto);
 		return produto;
 	}
