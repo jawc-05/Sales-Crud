9b04b4af5b630e2328e57510ab049547ce0e0475
jawc-05 <joaoalfredowc@gmail.com>
2026-04-30 15:11:12 +0200

    feat: improving SaleItem

--- src/main/java/br/com/jawc/domain/SaleItem.java
+++ src/main/java/br/com/jawc/domain/SaleItem.java
@@ -5,6 +5,8 @@ package br.com.jawc.domain;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "tb_sale_item")
 public class SaleItem {
@@ -13,4 +15,72 @@ public class SaleItem {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sale_item_seq")
     @SequenceGenerator(name="sale_item_seq", sequenceName = "seq_sale_item", initialValue = 1, allocationSize = 1)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "total_value")
+    private BigDecimal totalValue;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    private Sale sale;
+
+    public SaleItem() {
+        this.quantity = 0;
+        this.totalValue = BigDecimal.ZERO;
+    }
+
+    public void add(Integer quantity) {
+        this.quantity += quantity;
+
+        BigDecimal itemPrice = this.product.getPrice().multiply(BigDecimal.valueOf(quantity));
+
+        this.totalValue = this.totalValue.add(itemPrice);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(BigDecimal totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public Sale getSale() {
+        return sale;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
 }
+
