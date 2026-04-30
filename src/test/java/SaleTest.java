2ff32c84c3de17c6a806c0f2e3d03f6705caa145
jawc-05 <joaoalfredowc@gmail.com>
2026-04-30 17:39:29 +0200

    test: adding SaleTEst

--- src/test/java/SaleTest.java
+++ src/test/java/SaleTest.java
@@ -4,10 +4,14 @@ import br.com.jawc.dao.SaleDAO;
 import br.com.jawc.domain.Client;
 import br.com.jawc.domain.Product;
 import br.com.jawc.domain.Sale;
+import br.com.jawc.domain.SaleItem;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
 
 /**
  * @author jawc
@@ -25,7 +29,6 @@ public class SaleTest {
     Sale sale;
 
 
-
     @Before
     public void init(){
         this.saleDAO = new SaleDAO();
@@ -34,6 +37,52 @@ public class SaleTest {
 
         this.client = createClient();
         this.product = createProduct("FOOD-001");
+    }
+
+    @Test
+    public void saleTest(){
+         this.sale = new Sale();
+        sale.setClient(client);
+        sale.addProduct(product, 10);
+        sale.setStatus(Sale.Status.STARTED);
+        sale.setCode("S-0001");
+        sale.setSaleDate(Instant.now());
+        saleDAO.save(sale);
+
+        Assert.assertNotNull(sale.getId());
+
+        BigDecimal expectedValue = new BigDecimal("35.00");
+        Assert.assertTrue(expectedValue.compareTo(sale.getTotalValue()) == 0);
+
+        sale.addProduct(product, 2);
+        saleDAO.update(sale);
+        BigDecimal expectedValue2 = new BigDecimal("42.00");
+        Assert.assertTrue(expectedValue2.compareTo(sale.getTotalValue()) == 0);
+
+        Sale saleConsulted = saleDAO.findById(sale.getId());
+        Assert.assertEquals(saleConsulted.getId(), sale.getId());
+
+        Sale sale2 = new  Sale();
+        sale2.setClient(client);
+        sale2.setCode("S-0002");
+        sale2.setClient(createClient2());
+        sale2.addProduct(createProduct2("FOOD-0002"), 5);
+        sale2.setStatus(Sale.Status.STARTED);
+        sale2.setSaleDate(Instant.now());
+        saleDAO.save(sale2);
+        Assert.assertNotNull(sale2.getId());
+
+        List list =  saleDAO.findAll();
+        Assert.assertEquals(list.size(), 2);
+
+        sale.removeProduct(product, 2);
+        saleDAO.update(sale);
+        Sale saleConsulted2 = saleDAO.findById(sale.getId());
+
+        Assert.assertEquals(1, sale.getItems().size());
+        SaleItem item = sale.getItems().iterator().next();
+        Assert.assertEquals(Integer.valueOf(10), item.getQuantity());
+
     }
 
     private Client createClient(){
@@ -50,6 +99,20 @@ public class SaleTest {
         return client;
     }
 
+    private Client createClient2(){
+        Client client2 = new Client();
+
+        client2.setName("Cecília");
+        client2.setCpf("123456789");
+        client2.setTel(9999999999999l);
+        client2.setState("Rio Grande do SUl");
+        client2.setCity("Santa Maria");
+        client2.setAddress("Camobi");
+        client2.setNum(123);
+        clientDAO.save(client2);
+        return client2;
+    }
+
     private Product createProduct(String code){
         Product product = new Product();
         product.setName("Pasta");
@@ -58,5 +121,16 @@ public class SaleTest {
         product.setPrice(new BigDecimal(3.50));
         productDAO.save(product);
         return product;
+    }
+
+    private Product createProduct2(String code){
+        Product product2 = new  Product();
+
+        product2.setName("Rice");
+        product2.setDescription("Brazilian Rice");
+        product2.setCode(code);
+        product2.setPrice(new BigDecimal(5.45));
+        productDAO.save(product2);
+        return product2;
     }
 }
