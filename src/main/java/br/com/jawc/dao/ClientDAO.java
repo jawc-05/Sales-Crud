b059a3715796a0794c264787b0a74b9a6e94d5be
jawc-05 <joaoalfredowc@gmail.com>
2026-05-05 17:02:23 +0200

    docs: adding the stateless annotation in every dao class

--- src/main/java/br/com/jawc/dao/ClientDAO.java
+++ src/main/java/br/com/jawc/dao/ClientDAO.java
@@ -6,8 +6,9 @@ package br.com.jawc.dao;
 import br.com.jawc.dao.generic.GenericDAO;
 import br.com.jawc.dao.interfaces.IClientDAO;
 import br.com.jawc.domain.Client;
+import jakarta.ejb.Stateless;
 
-
+@Stateless
 public class ClientDAO extends GenericDAO<Client> implements IClientDAO {
 
 
--- src/main/java/br/com/jawc/dao/SaleDAO.java
+++ src/main/java/br/com/jawc/dao/SaleDAO.java
@@ -6,7 +6,9 @@ package br.com.jawc.dao;
 import br.com.jawc.dao.generic.GenericDAO;
 import br.com.jawc.dao.interfaces.ISaleDAO;
 import br.com.jawc.domain.Sale;
+import jakarta.ejb.Stateless;
 
+@Stateless
 public class SaleDAO extends GenericDAO<Sale> implements ISaleDAO {
     @Override
     public void finishSale(Sale sale) throws Exception {
