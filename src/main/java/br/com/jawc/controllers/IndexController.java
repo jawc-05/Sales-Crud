2b09fb63bff6b724793ca8218d8b90da1450fc38
jawc-05 <joaoalfredowc@gmail.com>
2026-05-07 16:42:37 +0200

    feat: clinet/list.xhtml is nice now

---
+++ src/main/java/br/com/jawc/controllers/IndexController.java
@@ -0,0 +1,20 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.controllers;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class IndexController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public String redirectClient() {
+        return "/client/list.xhtml?faces-redirect=true";
+    }
+}
